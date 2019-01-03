24e7d4a8457c28cac6cb54e28090f216e9fc1660
Kanogaelias <kanogae@gmail.com>
2019-01-03 11:53:04 +0300

    add url pattern for new profile

--- app/urls.py
+++ app/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns=[
        url(r'^$',views.home,name='Home'),
        url(r'^new_project',views.new_project,name='new_project'),
+       url(r'^new/profile$', views.new_profile, name='new-profile'),
        
