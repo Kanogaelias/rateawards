5ada95a8df348ec3a8bd728f1609fc6c881410ee
Kanogaelias <kanogae@gmail.com>
2019-01-03 17:10:25 +0300

    add foreign key and usability rating

--- app/models.py
+++ app/models.py
@@ -3,43 +3,48 @@ from django.contrib.auth.models import User
 # from django.core.validators import MaxValueValidator, MinValueValidator
 import numpy as np
 
-class Project(models.Model):
-    title=models.CharField(max_length=100)
-    description=models.CharField(max_length=250)
-    link=models.URLField(max_length=100)
-    project_image=models.ImageField(upload_to='projects/')
-    owner=models.ForeignKey(User,on_delete=models.CASCADE)
 
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=250)
+    link = models.URLField(max_length=100)
+    project_image = models.ImageField(upload_to='projects/')
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     @classmethod
-    def search_by_title(cls,search_term):
-        projects=cls.objects.filter(title__icontains=search_term)
+    def search_by_title(cls, search_term):
+        projects = cls.objects.filter(title__icontains=search_term)
         return projects
 
+    def average_rating(self):
+        all_ratings = list(map(lambda x: x.design_rating, self.rate_set.all()))
+        all_ratings = list(
+            map(lambda x: x.content_rating, self.rate_set.all()))
+        all_ratings = list(
+            map(lambda x: x.usability_rating, self.rate_set.all()))
+        return np.mean(all_ratings)
 
     def average_rating(self):
         all_ratings = list(map(lambda x: x.design_rating, self.rate_set.all()))
-        all_ratings = list(map(lambda x: x.content_rating, self.rate_set.all()))
-        all_ratings = list(map(lambda x: x.usability_rating, self.rate_set.all()))
+        all_ratings = list(
+            map(lambda x: x.content_rating, self.rate_set.all()))
+        all_ratings = list(
+            map(lambda x: x.usability_rating, self.rate_set.all()))
         return np.mean(all_ratings)
 
-
-    def average_rating(self):
-        all_ratings = list(map(lambda x: x.design_rating, self.rate_set.all()))
-        all_ratings = list(map(lambda x: x.content_rating, self.rate_set.all()))
-        all_ratings = list(map(lambda x: x.usability_rating, self.rate_set.all()))
-        return np.mean(all_ratings)
 
 class Profile(models.Model):
-    profile_pic=models.ImageField(upload_to='profile_photos/')
-    bio=models.CharField(max_length=300)
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    profile_pic = models.ImageField(upload_to='profile_photos/')
+    bio = models.CharField(max_length=300)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def save_profile(self):
         self.save()
+
     @classmethod
-    def search_by_profile(cls,search_term):
-        profiles=cls.objects.filter(user__icontains=search_term)
+    def search_by_profile(cls, search_term):
+        profiles = cls.objects.filter(user__icontains=search_term)
+
 
 class Rate(models.Model):
     RATING_CHOICES = (
@@ -52,5 +57,10 @@ class Rate(models.Model):
         (7, '7'),
         (8, '8'),
         (9, '9'),
-        (10,'10'),
+        (10, '10'),
     )
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    rater = models.ForeignKey(User, on_delete=models.CASCADE)
+    design_rating = models.IntegerField(choices=RATING_CHOICES, null=True)
+    usability_rating = models.IntegerField(choices=RATING_CHOICES, null=True)
+    content_rating = models.IntegerField(choices=RATING_CHOICES, null=True)
