5b787047cb71dfd505b59aa246828018e986196a
Kanogaelias <kanogae@gmail.com>
2019-01-03 16:18:00 +0300

    add home request and new_project request url

--- app/views.py
+++ app/views.py
@@ -2,3 +2,29 @@ from django.shortcuts import render,redirect,get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Project,Profile
 from .forms import NewProjectForm,NewProfileForm,RateForm
+
+@login_required(login_url='/accounts/login/')
+def home(request):
+    projects=Project.objects.all()
+    rate_form = RateForm()
+    return render(request,'index.html',{'projects':projects,'rate_form':rate_form})
+@login_required(login_url='/accounts/login/')
+def new_project(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewProjectForm(request.POST, request.FILES)
+        if form.is_valid():
+            project = form.save(commit=False)
+            project.owner = current_user
+            project.save()
+        return redirect('Home')
+
+    else:
+        form = NewProjectForm()
+    return render(request, 'new_project.html', {"form": form})
+@login_required(login_url="/accounts/login/")
+def profile(request):
+    profiles=Profile.objects.filter(user=request.user.id)
+    projects=Project.objects.filter(owner=request.user.id)
+    return render (request,'profile.html',{'projects':projects,'profiles':profiles,})
+@login_required(login_url="/accounts/login/")
