9f74a846ebc996a204d8f775d6dc363880db6f7d
Kanogaelias <kanogae@gmail.com>
2019-01-03 15:27:50 +0300

    import render template,auth decorators and profile

--- app/views.py
+++ app/views.py
@@ -1,2 +1,3 @@
-from django.shortcuts import render
-from django.contrib
+from django.shortcuts import render,redirect,get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import Project,Profile
