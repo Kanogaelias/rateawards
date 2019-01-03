7ecace059c3f7477595e2193fdb559b23c7d4825
Kanogaelias <kanogae@gmail.com>
2019-01-03 16:52:05 +0300

    add project class to return projects

--- app/models.py
+++ app/models.py
@@ -2,3 +2,16 @@ from django.db import models
 from django.contrib.auth.models import User
 # from django.core.validators import MaxValueValidator, MinValueValidator
 import numpy as np
+
+class Project(models.Model):
+    title=models.CharField(max_length=100)
+    description=models.CharField(max_length=250)
+    link=models.URLField(max_length=100)
+    project_image=models.ImageField(upload_to='projects/')
+    owner=models.ForeignKey(User,on_delete=models.CASCADE)
+
+
+    @classmethod
+    def search_by_title(cls,search_term):
+        projects=cls.objects.filter(title__icontains=search_term)
+        return projects
