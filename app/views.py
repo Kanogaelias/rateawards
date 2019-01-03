b9c62bf9f49f8e3b303fe79442bedc0f498f65ef
Kanogaelias <kanogae@gmail.com>
2019-01-03 16:10:43 +0300

    import forms

--- app/views.py
+++ app/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render,redirect,get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Project,Profile
+from .forms import NewProjectForm,NewProfileForm,RateForm
